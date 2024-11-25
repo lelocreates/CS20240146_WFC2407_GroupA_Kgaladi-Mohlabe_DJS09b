f4e8c18694d71d1357591d66dd6c3429ef76c0c0
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-11-25 11:07:43 +0200

    updating code for netlify

--- index.ts
+++ index.ts
@@ -74,7 +74,7 @@ const properties : {
     isAvailable: boolean;
 }[] = [
     {
-        image: './images/colombia-property.jpg',
+        image: './colombia-property.jpg',
         title: 'Colombian Shack',
         price: 45,
         location: {
@@ -87,7 +87,7 @@ const properties : {
         isAvailable: true  
     },
     {
-        image: './images/poland-property.jpg',
+        image: './poland-property.jpg',
         title: 'Polish Cottage',
         price: 30,
         location: {
@@ -100,7 +100,7 @@ const properties : {
         isAvailable: false 
     },
     {
-        image: './images/london-property.jpg',
+        image: './london-property.jpg',
         title: 'London Flat',
         price: 25,
         location: {
